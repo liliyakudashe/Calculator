9806d7633eb5881ae6f58e198187ef2b5b38a5c3
Acer <oduvan4ik_888@mail.ru>
2024-07-10 20:41:45 +0300

    Реализация класса Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите математическое выражение (или 'q' для выхода):");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                System.out.println("Выход из программы.");
+                break;
+            }
+            try {
+                String result = Calculator.calc(input);
+                System.out.println("Результат: " + result);
+            } catch (Exception e) {
+                System.out.println("throws Exception");
+            }
+            System.out.println("Введите следующее математическое выражение (или 'q' для выхода):");
+        }
+        scanner.close();
     }
 }
